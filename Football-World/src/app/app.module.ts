02ae6b5edf5eec2beb717bdcfd34cc2a31d7e142
Ivaylo Evtimov <evtimov9@gmail.com>
2018-05-22 15:44:57 +0300

    Bug fixex

--- Football-World/src/app/app.module.ts
+++ Football-World/src/app/app.module.ts
@@ -2,22 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import {AppRoutingModule} from './app-routing.module';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
